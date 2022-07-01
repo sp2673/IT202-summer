b5f3c22f0e05b8415be1b8e74f5976c4df1e29e7
sp2673 <sp2673@njit.edu>
2022-06-30 23:22:56 -0400

    Helper functions

--- public_html/Project/register.php
+++ public_html/Project/register.php
@@ -36,9 +36,14 @@ if (isset($_POST["email"]) && isset($_POST["password"]) && isset($_POST["confirm
         $hasError = true;
     }
     //sanitize
-    $email = filter_var($email, FILTER_SANITIZE_EMAIL);
-    if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+    //$email = filter_var($email, FILTER_SANITIZE_EMAIL);
+    $email = sanitize_email($email);
+    /*if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
         echo "Invalid email";
+        $hasError = true;
+    }*/
+    if (!is_valid_email($email)){
+        echo "Please enter a valid email <br>";
         $hasError = true;
     }
     if (empty($password)) {
