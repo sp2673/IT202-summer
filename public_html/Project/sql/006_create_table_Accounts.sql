7b7ca1611bb6c59f23475728a1552b75d43c4550
sp2673 <sp2673@njit.edu>
2022-07-21 12:32:16 -0400

    Creating Account

--- public_html/Project/sql/006_create_table_Accounts.sql
+++ public_html/Project/sql/006_create_table_Accounts.sql
@@ -1,9 +1,10 @@
 CREATE TABLE IF NOT EXISTS Accounts(
+    //bank accounts
     id int AUTO_INCREMENT PRIMARY KEY,
-    account_number VARCHAR(12) UNIQUE ,
+    account_number VARCHAR(12) NOT NULL ,
     user_id int ,
-    balance int NOT NULL DEFAULT 0,
-    account_type VARCHAR(255) NOT NULL ,
+    balance decimal(12,2) DEFAULT 0.00,
+    account_type VARCHAR(255),
     created TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     modified TIMESTAMP DEFAULT CURRENT_TIMESTAMP on update CURRENT_TIMESTAMP,
 
