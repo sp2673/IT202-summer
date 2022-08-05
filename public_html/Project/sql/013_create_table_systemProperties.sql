c1822cfeba0efd731c9e5564ead33a42b273d97e
sp2673 <106455418+sp2673@users.noreply.github.com>
2022-08-05 12:48:13 -0400

    Delete 013_create_table_systemProperties.sql

--- public_html/Project/sql/013_create_table_systemProperties.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE IF NOT EXISTS `System Properties`
-(
-    id             int auto_increment,
-    first_updated   TIMESTAMP default CURRENT_TIMESTAMP,
-    last_updated TIMESTAMP default CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-    APY            decimal (12,2) default 0.00,
-    PRIMARY KEY (id)
-)
